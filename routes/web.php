fa845b8e86255e3bcb11e934770c2946d49c4029
Rama Faisal <helloramafaisal@gmail.com>
2025-02-17 15:33:55 +0700

    Init Commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('admin', function() {
+    return '<h1>Halo Admin</h1>';
+})->middleware(['auth', 'verified', 'role:admin']);
+
+Route::get('penulis', function() {
+    return '<h1>Halo Penulis</h1>';
+})->middleware(['auth', 'verified', 'role:penulis|admin']);
+
+Route::get('tulisan', function() {
+    return view('tulisan');
+})->middleware(['auth', 'verified', 'role_or_permission:lihat-tulisan|admin']);
+
+require __DIR__.'/auth.php';
